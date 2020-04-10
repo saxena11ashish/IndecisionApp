be434cd1817f47a79f0b6c478016794ed7e68e42
saxena11ashish <ashishsxn96@gmail.com>
2020-04-10 10:32:48 +0530

    .

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,82 +1,22 @@
 'use strict';
 
-console.log('App.js is running');
+var nameVar = 'ashish';
+var nameVar = 'ashish saxena';
+console.log('nameVar', nameVar);
 
-var app = {
-  title: 'Indecision App',
-  subTitle: 'Let the computer decide what you do',
-  options: ['one', 'two']
-};
+var nameLet = 'nigga';
+nameLet = 'ak';
+console.log('nameLet', nameLet);
 
-// JSX - JavaScript XML
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.title
-  ),
-  app.subTitle && React.createElement(
-    'p',
-    null,
-    app.subTitle
-  ),
-  React.createElement(
-    'p',
-    null,
-    app.options.length > 0 ? 'Here are your options' : 'No options'
-  ),
-  React.createElement(
-    'ol',
-    null,
-    React.createElement(
-      'li',
-      null,
-      'Item one'
-    ),
-    React.createElement(
-      'li',
-      null,
-      'Item two'
-    )
-  )
-);
+var nameConst = 'YO';
+console.log('nameConst', nameConst);
 
-var user = {
-  name: 'ashish',
-  age: 23,
-  location: 'New Delhi-59'
-};
-function getLocation(location) {
-  if (location) {
-    return React.createElement(
-      'p',
-      null,
-      'Location: ',
-      location
-    );
-  }
+//Block Scoping
+
+var fullName = "Ashish Saxena";
+var firstName = void 0;
+if (fullName) {
+    firstName = fullName.split(' ')[0];
+    console.log(firstName);
 }
-
-var templateTwo = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    user.name ? user.name : 'Anonymous'
-  ),
-  user.age && user.age >= 18 && React.createElement(
-    'p',
-    null,
-    'Age: ',
-    user.age
-  ),
-  getLocation(user.location)
-);
-
-var appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);
-// ReactDOM.render(templateTwo, appRoot);
+console.log(firstName);
